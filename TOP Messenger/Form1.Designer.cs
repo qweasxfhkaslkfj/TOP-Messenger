be60f21ce6d188542e0f55e3e90049582e5a8f3e
Nas <duhinaanastasia197.@gmail.com>
2025-12-03 15:55:35 +0300

    реализация хранения данных

--- TOP Messenger/Form1.Designer.cs
+++ TOP Messenger/Form1.Designer.cs
@@ -2,39 +2,113 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ListBox lstMessages;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnStartServer;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnChangeName;
 
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
 
-        #region Код, автоматически созданный конструктором форм Windows
 
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+            this.lstMessages = new System.Windows.Forms.ListBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnStartServer = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnChangeName = new System.Windows.Forms.Button();
+            this.SuspendLayout();
 
-        #endregion
+            // lstMessages
+            this.lstMessages.FormattingEnabled = true;
+            this.lstMessages.ItemHeight = 16;
+            this.lstMessages.Location = new System.Drawing.Point(12, 41);
+            this.lstMessages.Name = "lstMessages";
+            this.lstMessages.Size = new System.Drawing.Size(560, 340);
+            this.lstMessages.TabIndex = 0;
+
+            // txtMessage
+            this.txtMessage.Location = new System.Drawing.Point(12, 390);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(460, 22);
+            this.txtMessage.TabIndex = 1;
+            this.txtMessage.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMessage_KeyPress);
+
+            // btnSend
+            this.btnSend.Location = new System.Drawing.Point(478, 389);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(94, 25);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+
+            // btnStartServer
+            this.btnStartServer.Location = new System.Drawing.Point(12, 12);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(120, 23);
+            this.btnStartServer.TabIndex = 3;
+            this.btnStartServer.Text = "Запустить сервер";
+            this.btnStartServer.UseVisualStyleBackColor = true;
+            this.btnStartServer.Click += new System.EventHandler(this.btnStartServer_Click);
+
+            // txtIP
+            this.txtIP.Location = new System.Drawing.Point(138, 13);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(150, 22);
+            this.txtIP.TabIndex = 4;
+            this.txtIP.Text = "127.0.0.1";
+
+            // btnConnect
+            this.btnConnect.Location = new System.Drawing.Point(294, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(94, 23);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "Подключиться";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+
+            // label1
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(394, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "(IP другого пользователя)";
+
+            // btnChangeName
+            this.btnChangeName.Location = new System.Drawing.Point(478, 12);
+            this.btnChangeName.Name = "btnChangeName";
+            this.btnChangeName.Size = new System.Drawing.Size(94, 23);
+            this.btnChangeName.TabIndex = 7;
+            this.btnChangeName.Text = "Сменить имя";
+            this.btnChangeName.UseVisualStyleBackColor = true;
+            this.btnChangeName.Click += new System.EventHandler(this.btnChangeName_Click);
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 426);
+            this.Controls.Add(this.btnChangeName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnStartServer);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.lstMessages);
+            this.Name = "Form1";
+            this.Text = "Чат";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
     }
 }
-
---
+++ TOP Messenger/Saveing_Meeseg.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace TOP_Messenger
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener server;
+        private List<ClientHandler> clients = new List<ClientHandler>();
+        private Thread serverThread;
+        private bool isRunning = false;
+        private string userName = "User1"; // Имя текущего пользователя
+
+      
+        // Класс для обработки каждого клиента
+        public class ClientHandler
+        {
+            public TcpClient Client { get; set; }
+            public string UserName { get; set; }
+            public NetworkStream Stream { get; set; }
+            public StreamReader Reader { get; set; }
+            public StreamWriter Writer { get; set; }
+            public Thread ClientThread { get; set; }
+        }
+
+        // Запуск сервера
+        private void StartServer()
+        {
+            try
+            {
+                server = new TcpListener(IPAddress.Any, 8888);
+                server.Start();
+                isRunning = true;
+
+                Invoke((MethodInvoker)delegate
+                {
+                    lstMessages.Items.Add($"Сервер запущен на порту 8888");
+                    lstMessages.Items.Add($"Ваше имя: {userName}");
+                });
+
+                while (isRunning)
+                {
+                    if (server.Pending())
+                    {
+                        TcpClient client = server.AcceptTcpClient();
+                        ClientHandler handler = new ClientHandler
+                        {
+                            Client = client,
+                            Stream = client.GetStream(),
+                            UserName = "Unknown"
+                        };
+                        handler.Reader = new StreamReader(handler.Stream, Encoding.UTF8);
+                        handler.Writer = new StreamWriter(handler.Stream, Encoding.UTF8);
+
+                        // Получаем имя пользователя
+                        handler.UserName = handler.Reader.ReadLine();
+
+                        Invoke((MethodInvoker)delegate
+                        {
+                            lstMessages.Items.Add($"{handler.UserName} подключился");
+                        });
+
+                        // Добавляем в список клиентов
+                        clients.Add(handler);
+
+                        // Запускаем поток для чтения сообщений от этого клиента
+                        handler.ClientThread = new Thread(() => HandleClient(handler));
+                        handler.ClientThread.Start();
+                    }
+                    Thread.Sleep(100);
+                }
+            }
+            catch (Exception ex)
+            {
+                Invoke((MethodInvoker)delegate
+                {
+                    lstMessages.Items.Add($"Ошибка сервера: {ex.Message}");
+                });
+            }
+        }
+
+        // Обработка сообщений от клиента
+        private void HandleClient(ClientHandler handler)
+        {
+            try
+            {
+                while (isRunning && handler.Client.Connected)
+                {
+                    if (handler.Stream.DataAvailable)
+                    {
+                        string message = handler.Reader.ReadLine();
+                        if (message != null)
+                        {
+                            // Отображаем сообщение
+                            Invoke((MethodInvoker)delegate
+                            {
+                                lstMessages.Items.Add($"[{handler.UserName}]: {message}");
+                                lstMessages.SelectedIndex = lstMessages.Items.Count - 1;
+                            });
+
+                            // Пересылаем сообщение всем клиентам
+                            BroadcastMessage($"[{handler.UserName}]: {message}");
+                        }
+                    }
+                    Thread.Sleep(50);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка обработки клиента: {ex.Message}");
+            }
+            finally
+            {
+                // Удаляем клиента при отключении
+                RemoveClient(handler);
+            }
+        }
+
+        // Отправка сообщения всем клиентам
+        private void BroadcastMessage(string message)
+        {
+            lock (clients)
+            {
+                foreach (var client in clients.ToList())
+                {
+                    try
+                    {
+                        if (client.Client.Connected)
+                        {
+                            client.Writer.WriteLine(message);
+                            client.Writer.Flush();
+                        }
+                    }
+                    catch
+                    {
+                        RemoveClient(client);
+                    }
+                }
+            }
+        }
+
+        // Удаление клиента
+        private void RemoveClient(ClientHandler handler)
+        {
+            lock (clients)
+            {
+                if (clients.Contains(handler))
+                {
+                    Invoke((MethodInvoker)delegate
+                    {
+                        lstMessages.Items.Add($"{handler.UserName} отключился");
+                    });
+
+                    clients.Remove(handler);
+                    handler.Client.Close();
+                }
+            }
+        }
+
+        // Подключение к другому серверу (чтобы два пользователя могли общаться)
+        private void ConnectToServer(string ipAddress, string userName)
+        {
+            Thread connectThread = new Thread(() =>
+            {
+                try
+                {
+                    TcpClient client = new TcpClient();
+                    client.Connect(ipAddress, 8888);
+
+                    NetworkStream stream = client.GetStream();
+                    StreamWriter writer = new StreamWriter(stream, Encoding.UTF8);
+                    StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+
+                    // Отправляем имя пользователя
+                    writer.WriteLine(userName);
+                    writer.Flush();
+
+                    // Добавляем в список как клиента
+                    ClientHandler handler = new ClientHandler
+                    {
+                        Client = client,
+                        Stream = stream,
+                        Reader = reader,
+                        Writer = writer,
+                        UserName = userName
+                    };
+
+                    lock (clients)
+                    {
+                        clients.Add(handler);
+                    }
+
+                    Invoke((MethodInvoker)delegate
+                    {
+                        lstMessages.Items.Add($"Подключились к {ipAddress} как {userName}");
+                    });
+
+                    // Читаем входящие сообщения
+                    while (client.Connected)
+                    {
+                        try
+                        {
+                            if (stream.DataAvailable)
+                            {
+                                string message = reader.ReadLine();
+                                if (message != null)
+                                {
+                                    Invoke((MethodInvoker)delegate
+                                    {
+                                        lstMessages.Items.Add(message);
+                                        lstMessages.SelectedIndex = lstMessages.Items.Count - 1;
+                                    });
+                                }
+                            }
+                            Thread.Sleep(50);
+                        }
+                        catch
+                        {
+                            break;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Invoke((MethodInvoker)delegate
+                    {
+                        lstMessages.Items.Add($"Ошибка подключения: {ex.Message}");
+                    });
+                }
+            });
+            connectThread.Start();
+        }
+
+        // Отправка сообщения
+        private void SendMessage(string message)
+        {
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                string fullMessage = $"[{userName}]: {message}";
+
+                // Отображаем свое сообщение
+                lstMessages.Items.Add(fullMessage);
+                lstMessages.SelectedIndex = lstMessages.Items.Count - 1;
+
+                // Отправляем всем подключенным клиентам
+                BroadcastMessage(fullMessage);
+
+                txtMessage.Clear();
+                txtMessage.Focus();
+            }
+        }
+
+        // Кнопка запуска сервера
+        private void btnStartServer_Click(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                serverThread = new Thread(StartServer);
+                serverThread.Start();
+                btnStartServer.Text = "Сервер запущен";
+                btnStartServer.Enabled = false;
+            }
+        }
+
+        // Кнопка подключения к серверу
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            string ip = txtIP.Text;
+            if (!string.IsNullOrWhiteSpace(ip))
+            {
+                ConnectToServer(ip, userName);
+            }
+        }
+
+        // Кнопка отправки сообщения
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            SendMessage(txtMessage.Text);
+        }
+
+        // Отправка по Enter
+        private void txtMessage_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                SendMessage(txtMessage.Text);
+                e.Handled = true;
+            }
+        }
+
+        // Очистка при закрытии
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            isRunning = false;
+
+            // Отключаем всех клиентов
+            lock (clients)
+            {
+                foreach (var client in clients)
+                {
+                    try
+                    {
+                        client.Client.Close();
+                    }
+                    catch { }
+                }
+                clients.Clear();
+            }
+
+            // Останавливаем сервер
+            if (server != null)
+            {
+                server.Stop();
+            }
+        }
+
+        // Смена имени пользователя
+        private void btnChangeName_Click(object sender, EventArgs e)
+        {
+            string newName = Microsoft.VisualBasic.Interaction.InputBox(
+                "Введите новое имя:", "Смена имени", userName);
+
+            if (!string.IsNullOrWhiteSpace(newName))
+            {
+                userName = newName;
+                this.Text = "Чат - " + userName;
+                lstMessages.Items.Add($"Имя изменено на: {userName}");
+            }
+        }
+    }
+}
